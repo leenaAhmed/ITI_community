b3388ad829e91373d808e8364e225db0a2beb75b
leenaAhmed <leena3792@gmail.com>
2021-12-22 23:08:35 +0200

    add new request

--- js/login.js
+++ js/login.js
@@ -1,29 +1,33 @@
+var newreq = new XMLHttpRequest();
+console.log("loading request");
+var data;
+
+newreq.open("GET", "./json/profile.json");
+newreq.addEventListener("readystatechange", function () {
+  if (newreq.readyState == 4 && newreq.status == 200) {
+    data = JSON.parse(newreq.response);
+
+    console.log(data);
+  }
+});
 
 //button of sin up and button of log in on page sign in
-document.getElementById("login_sign_in").addEventListener("click",function(e){
+document
+  .getElementById("login_sign_in")
+  .addEventListener("click", function (e) {
     e.preventDefault();
-    document.getElementById("login_sign_in").style.cursor="not-allowed";
-
-});
-document.getElementById("login_sign_up").addEventListener("click",function(e){
-  e.preventDefault();
-  window.open("index.html","_self");
-});
+    document.getElementById("login_sign_in").style.cursor = "not-allowed";
+  });
+document
+  .getElementById("login_sign_up")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    window.open("index.html", "_self");
+  });
 
 //form
 var form2 = document.getElementById("form_log");
 var _email = document.getElementById("Email_log").Value;
 var _passward = document.getElementById("Passward_log").Value;
 
-var xhr = new XMLHttpRequest();
-
-var data;
-
-xhr.open("GET", "./json/profile.json");
-xhr.addEventListener("readystatechange", function () {
-  if (xhr.readyState == 4 && xhr.status == 200) {
-    data = JSON.parse(xhr.response);
-    console.log(data.users);
-   
-  }
-});
+newreq.send("");
