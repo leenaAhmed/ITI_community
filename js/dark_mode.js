e79b6d9a41249252eb055f203a7542673128e022
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2021-12-23 22:57:56 +0200

    dark-mode
    محتاج نضيفه فى ال profile and home

---
+++ js/dark_mode.js
@@ -0,0 +1,13 @@
+var toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click',function(e) {
+    
+    var html = document.querySelector('html')
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
