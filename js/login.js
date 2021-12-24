51c4ea97c71125601d0fd3195154b1a0f4690e2f
Sondos1233 <69219643+Sondos1233@users.noreply.github.com>
2021-12-24 19:02:10 +0200

    Update login.js

--- js/login.js
+++ js/login.js
@@ -34,12 +34,12 @@ logIn.addEventListener("click", function (e) {
     for(var i=0; i<arr.length;i++){
       if(arr[i] == email.value){
         window.open("profile.html");
-      }else{
+      }/*else{
         if (confirm("you are not have an account/Are you want sign Up? ") == true) {
           window.open("index.html");
-        }
         
-      }
+        
+      }*/
     }
   }
 
