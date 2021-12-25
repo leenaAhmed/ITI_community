8af5c7f3c76d6c7d7ee66590325716099e251a80
leenaAhmed <leena3792@gmail.com>
2021-12-25 02:28:19 +0200

    add users posts

--- js/post.js
+++ js/post.js
@@ -26,3 +26,34 @@ function previewFiles() {
     [].forEach.call(PostImage, readAndPreview);
   }
 }
+
+//
+function getcookies() {
+  var associativeCookie = [];
+  var cookies = document.cookie.split(";"); //
+
+  for (var i = 0; i < cookies.length; i++) {
+    associativeCookie[cookies[i].split("=")[0].trim()] =
+      cookies[i].split("=")[1];
+    console.log(cookies[i].split("=")[1]);
+  }
+  return associativeCookie;
+}
+var newProfileImg = document.getElementById("homeProfileImg");
+var homeprofileImg = document.getElementById("profileFullName");
+
+function displaycookie() {
+  var getcookie = getcookies();
+  // console.log(getcookie);
+  var _profilenam = getcookie["firstName"];
+  var _profileLnam = getcookie["lastName"];
+  homeprofileImg.innerHTML += `${_profilenam} ${_profileLnam} `;
+  //
+  var _newProfileImg = getcookie["EditProfileImg"];
+
+  var profile = _newProfileImg.replace(/C:\\fakepath\\/g, "");
+  newProfileImg.src = `/assest/image/${profile}`;
+
+  // console.log(_profilenam);
+}
+displaycookie();
